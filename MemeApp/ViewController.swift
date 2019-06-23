8bf8199a2850bd4d5655a552609b32c35e251d2f
Hernand Azevedo <hernand.azevedo@gmail.com>
2019-06-23 10:48:33 -0300

    Initial Commit

---
+++ MemeApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MemeApp
+//
+//  Created by Hernand Azevedo on 23/06/19.
+//  Copyright © 2019 Hernand Azevedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UIImagePickerControllerDelegate{
+
+    @IBOutlet weak var imageView: UIImageView!
+    var imagePicker: ImagePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+    }
+
+    @IBAction func pickImage(_ sender: UIButton) {
+        self.imagePicker.present(from: sender)
+    }
+    
+}
+
+extension ViewController: ImagePickerDelegate {
+    
+    func didSelect(image: UIImage?) {
+        self.imageView.image = image
+    }
+}
