6adf0482b102957d5181492070bb2d172f6b9db0
Hernand Azevedo <hernand.azevedo@gmail.com>
2019-06-29 11:49:54 -0300

    :lipstick: Adjust text field style

--- MemeApp/ViewController.swift
+++ MemeApp/ViewController.swift
@@ -13,9 +13,27 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate{
     @IBOutlet weak var imageView: UIImageView!
     var imagePicker: ImagePicker!
     
+    @IBOutlet weak var textFieldTop: UITextField!
+    @IBOutlet weak var textFieldBottom: UITextField!
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  -2.0
+    ]
+    fileprivate func configureTextFields() {
+        self.textFieldTop.delegate = self
+        self.textFieldTop.defaultTextAttributes = memeTextAttributes
+        self.textFieldTop.borderStyle = .none
+        self.textFieldBottom.delegate = self
+        self.textFieldBottom.defaultTextAttributes = memeTextAttributes
+        self.textFieldBottom.borderStyle = .none
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+        configureTextFields()
     }
 
     @IBAction func pickImage(_ sender: UIButton) {
@@ -27,5 +45,17 @@ extension ViewController: ImagePickerDelegate {
     
     func didSelect(image: UIImage?) {
         self.imageView.image = image
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
     }
 }
